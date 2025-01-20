12c5db5f466c8c794e0f5f1cb96a713d4666893c
L A <laurenceunabia@gmail.com>
2025-01-20 20:29:14 +0800

    update height image upload

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -103,7 +103,7 @@ const ImageUpload = ({
           alt={file.filePath}
           path={file.filePath}
           width={500}
-          height={500}
+          height={300}
         />
       )}
     </ImageKitProvider>
